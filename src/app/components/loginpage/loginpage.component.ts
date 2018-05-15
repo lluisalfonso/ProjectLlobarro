c6a56aea472d3f06252ee9df7d7e5c21145b2761
Andreu Plana <andreuplana@gmail.com>
2018-05-15 09:24:00 +0200

    Login with superchuli navbar
    Login Logout funcs from the master of disaster @andrestionen IG

--- src/app/components/loginpage/loginpage.component.ts
+++ src/app/components/loginpage/loginpage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-loginpage',
   templateUrl: './loginpage.component.html',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginpageComponent implements OnInit {
 
-  constructor() { }
+  public email : string;
+  public password : string;
+
+  constructor(
+    public authService : AuthService,
+    public router : Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmitLogin(){
+    this.authService.loginEmail(this.email,this.password)
+    .then( (res)=>{
+      this.router.navigate(['/private']);
+    }).catch((err)=>{
+      console.log(err);
+      this.router.navigate(['/login']);
+    });
+
   }
 
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public isLogin : boolean;
+  public username : string;
+  public email : string;
+
+  constructor(
+    public authService : AuthService,
+
+  ) { }
 
   ngOnInit() {
-  }
+    this.authService.getAuth().subscribe(autho=>{
+      if(autho){
+        this.isLogin=true;
+        this.email = autho.email;
+        this.username = autho.displayName
+      }else{
+        this.isLogin=false;
+      }
 
+    })
+  }
+  onClickLogout(){
+    this.authService.logout();
+  }
 }
